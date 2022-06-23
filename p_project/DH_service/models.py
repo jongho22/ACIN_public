5ecf619f54188e6ce7d5beccd307b3618bbab3a9
shin_jong_ho <sjh0804_22@naver.com>
2022-06-23 12:33:22 +0900

    'keyword_time_add_to_DB'

--- p_project/DH_service/models.py
+++ p_project/DH_service/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# 검색어와 검색 기간을 입력하면 이곳으로 저장
 class SEND_DATA(models.Model):
-    #title = models.CharField(max_length=50)
     text = models.CharField(max_length=10)
-    start_day = models.CharField(max_length=8)
-    end_day = models.CharField(max_length=8)
+    start_day = models.DateField()
+    end_day = models.DateField()
     def __str__(self):
         return self.text
--- p_project/DH_service/views.py
+++ p_project/DH_service/views.py
@@ -2,19 +2,33 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from .models import SEND_DATA
+from django.utils import timezone
 # Create your views here.
 
-# 메인화면 
+# 메인화면 데이터 전송
 def Home(request) :
 
     if request.method == "POST":
         
+        # 입력 값 받아오기
         temp = request.POST.get('keyword_input')
+        start = request.POST.get('start')
+        end = request.POST.get('end')
 
+        # 입력 값 전달 
         keyword = SEND_DATA()
-    
+
         keyword.text = temp
+        keyword.start_day = start
+        keyword.end_day = end
         
+        # 입력 값 확인
+        #print('-'*20)
+        #print(keyword.start_day)
+        #print(keyword.end_day)
+        #print('-'*20)
+
+        # DB에 저장
         keyword.save()
         
         keyword_list = SEND_DATA.objects.all()
